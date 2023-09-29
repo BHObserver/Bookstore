766f276a1d08aea6a84bedf55411cc8443f8e213
BHObserver <BHObserver0@gmail.com>
2023-09-30 03:04:06 +0600

    Create BookForm.js

---
+++ src/components/BookForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const BookForm = ({ onSubmit }) => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ title, author });
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <button type="submit">Add Book</button>
+    </form>
+  );
+};
+
+// Prop validation
+BookForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired, // onSubmit should be a function and is required
+};
+
+export default BookForm;
