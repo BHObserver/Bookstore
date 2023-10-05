f122cb4b5647914ecc81727b18e2b76bced67fea
BHObserver <BHObserver0@gmail.com>
2023-10-06 05:32:45 +0600

    Update BookForm.js

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,13 +1,33 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
-const BookForm = ({ onSubmit }) => {
+const BookForm = () => {
+  const books = useSelector((state) => state.books.books);
+  const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
 
+  const handleTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleAuthor = (event) => {
+    setAuthor(event.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    onSubmit({ title, author });
+    const newBook = {
+      item_id: `item${books.length + 1}`,
+      title,
+      author,
+    };
+
+    // Dispatch the addBook action with the new book
+    dispatch(addBook(newBook));
+
+    // Clear the form inputs
     setTitle('');
     setAuthor('');
   };
@@ -18,22 +38,17 @@ const BookForm = ({ onSubmit }) => {
         type="text"
         placeholder="Title"
         value={title}
-        onChange={(e) => setTitle(e.target.value)}
+        onChange={handleTitle}
       />
       <input
         type="text"
         placeholder="Author"
         value={author}
-        onChange={(e) => setAuthor(e.target.value)}
+        onChange={handleAuthor}
       />
       <button type="submit">Add Book</button>
     </form>
   );
-};
-
-// Prop validation
-BookForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired, // onSubmit should be a function and is required
 };
 
 export default BookForm;
