4df24603a51d41c5bf8e9095b33ed3219cb6ef64
BHObserver <BHObserver0@gmail.com>
2023-10-08 05:09:28 +0600

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import BookList from '../components/BookList';
 import BookForm from '../components/BookForm';
-import { addBook, removeBook } from '../redux/books/booksSlice';
+import { addBook } from '../redux/books/booksSlice';
 
 function HomePage() {
   const dispatch = useDispatch();
-  const books = useSelector((state) => state.books);
 
   const handleAddBook = (newBook) => {
     dispatch(addBook(newBook));
   };
 
-  const handleDeleteBook = (id) => {
-    dispatch(removeBook(id));
-  };
-
   return (
     <div>
       <BookForm onSubmit={handleAddBook} />
-      <BookList books={books} onDelete={handleDeleteBook} />
+      <BookList />
     </div>
   );
 }
