7b24009baa94718431703184df85e6f94fbc6fc5
BHObserver <BHObserver0@gmail.com>
2023-10-11 23:17:12 +0600

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -13,54 +13,56 @@ const Book = ({ book }) => {
   };
 
   return (
-    <div className="book-details">
-      <div className="book-content">
-        <div className="book-meta">
-          <h4 className="book-genre">{book.category}</h4>
-          <h2 className="book-title">{book.title}</h2>
-          <h6 className="book-author-name">{book.author}</h6>
-          <div className="action-buttons">
-            <button type="button" className="button-outline">Comments</button>
-            <div className="vertical-divider" />
-            <button
-              type="button"
-              className="button-outline"
-              onClick={handleDeleteBook}
-            >
-              Delete
-            </button>
-            <div className="vertical-divider" />
-            <button type="button" className="button-outline">Edit</button>
-          </div>
-        </div>
-        <div className="progress-container">
-          <div className="circular-progress-container">
-            <div className="circular-progress-bar" />
-          </div>
-          <div className="progress-stat">
-            <p className="percent-complete">
-              { Math.floor(Math.random() * (75 - 40 + 1)) + 40 }
-              %
-            </p>
-            <p className="completed-label">Completed</p>
-          </div>
-          <div className="progress-divider" />
-          <div className="current-chapter-info">
-            <div>
-              <p className="chapter-label">CURRENT CHAPTER</p>
-              <p className="current-chapter">
-                Chapter
-                {' '}
-                { Math.floor(Math.random() * 30) + 1 }
-              </p>
+    <li className="book-card" key={book.item_id} id={book.item_id}>
+      <div className="book-details">
+        <div className="book-content">
+          <div className="book-meta">
+            <h4 className="book-genre">{book.category}</h4>
+            <h2 className="book-title">{book.title}</h2>
+            <h6 className="book-author-name">{book.author}</h6>
+            <div className="action-buttons">
+              <button type="button" className="button-outline">Comments</button>
+              <div className="vertical-divider" />
+              <button
+                type="button"
+                className="button-outline"
+                onClick={handleDeleteBook}
+              >
+                Delete
+              </button>
+              <div className="vertical-divider" />
+              <button type="button" className="button-outline">Edit</button>
             </div>
-            <div>
-              <button type="button" className="primary-action-button">UPDATE PROGRESS</button>
+          </div>
+          <div className="progress-container">
+            <div className="circular-progress-container">
+              <div className="circular-progress-bar" />
+            </div>
+            <div className="progress-stat">
+              <p className="percent-complete">
+                { Math.floor(Math.random() * (75 - 40 + 1)) + 40 }
+                %
+              </p>
+              <p className="completed-label">Completed</p>
+            </div>
+            <div className="progress-divider" />
+            <div className="current-chapter-info">
+              <div>
+                <p className="chapter-label">CURRENT CHAPTER</p>
+                <p className="current-chapter">
+                  Chapter
+                  {' '}
+                  { Math.floor(Math.random() * 30) + 1 }
+                </p>
+              </div>
+              <div>
+                <button type="button" className="primary-action-button">UPDATE PROGRESS</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </li>
   );
 };
 
